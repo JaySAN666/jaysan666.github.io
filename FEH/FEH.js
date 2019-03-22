6f2e9f335057e0dda8dd5b0bde9a5e22cfe9dbf7
JaySAN666 <38299411+JaySAN666@users.noreply.github.com>
2019-03-21 19:56:36 -0700

    Add files via upload

--- FEH/FEH.js
+++ FEH/FEH.js
@@ -1,6 +1,39 @@
-//Place Holder File
-$(document).ready(function(){
-  $("button").click(function(){
-    $("#test").show();
-  });
-});
+
+var end_tap = 3;
+var end_gc = 19;
+var end_tt = 2;
+var end_vg = 6;
+var end_fb = 22;
+
+var month1  = "January";
+var month2  = "February";
+var month3  = "March";
+var month4  = "April";
+var month5  = "May";
+var month6  = "June";
+var month7  = "July";
+var month8  = "August";
+var month9  = "September";
+var month10 = "October";
+var month11 = "November";
+var month12 = "December";
+
+function tap_battle(){
+	window.alert("Tap Battle will end on " + month4 + " " + end_tap);
+}
+
+function grand_conquests(){
+	window.alert("Grand Conquests will end on " + month3 + " " + end_gc);
+}
+
+function tempest_trials(){
+	window.alert("Tempest Trials will end on " + month4 + " " + end_tt);
+}
+
+function voting_gauntlet(){
+	window.alert("Voting Gauntlet will end on " + month3 + " " + end_vg);
+}
+
+function forging_bonds(){
+	window.alert("Forging Bonds will end on " + month3 + " " + end_fb);
+}
